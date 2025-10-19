f410892cefec6f9cfa98cd9e62f63ce050bdd68d
Aishwarya Chhablani <chhablaniaishwarya@gmail.com>
2025-10-18 21:56:24 -0400

    .

--- src/store/index.js
+++ src/store/index.js
@@ -1,7 +1,24 @@
-import {configureStore} from '@reduxjs/toolkit';
+import { configureStore } from '@reduxjs/toolkit';
 import todosReducer from './todosSlice';
-export const store = configureStore({
-reducer: {
-todos: todosReducer,
-},
+
+const store = configureStore({
+  reducer: {
+    todos: todosReducer, // your slice
+  },
+  devTools: true, // Redux DevTools enabled automatically
 });
+// Log Redux state on every change
+store.subscribe(() => {
+  console.log('Redux State:', store.getState());
+});
+// Log Redux state on every change
+store.subscribe(() => {
+  console.log('Redux State:', store.getState());
+});
+
+// Log Redux state on every change
+store.subscribe(() => {
+  console.log('Redux State:', store.getState());
+});
+
+export default store;
